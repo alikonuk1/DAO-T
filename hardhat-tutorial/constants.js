0125348c27d659d9d46c0a4c9ba4602a80ff0eb3
ali konuk <alikonuk1@gmail.com>
2022-02-16 00:55:08 +0300

    1st commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x567e2c4a5FDf96Db5Fd552df13Caea9B5A0EF6b7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
